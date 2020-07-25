b35a6e41f3656d1a5c915ee15d152d2cdc93d1ca
camrbuss <camrbuss@vt.edu>
2020-07-24 18:10:08 -0600

    Changed keymap transform to make physical sense

--- keyboards/kobuss/camro/camro.h
+++ keyboards/kobuss/camro/camro.h
@@ -2,10 +2,11 @@
 
 #include "quantum.h"
 
+//      KEEB: ENCS,  KEY0,  KEY1,  KEY2
+// QMK ARRAY: KEY0,  KEY2,  KEY1,  ENCS
 #define LAYOUT_default( \
-  K00, K01, \
-  K02, K03  \
+  K00, K01, K02, K03  \
 ) { \
-  { K00,  K01 }, \
-  { K02,  K03 }, \
+  { K01,  K03 }, \
+  { K02,  K00 }, \
 }
--- keyboards/kobuss/camro/keymaps/default/keymap.c
+++ keyboards/kobuss/camro/keymaps/default/keymap.c
@@ -1,13 +1,14 @@
 #include QMK_KEYBOARD_H
 
+// KEEB: ENCS,  KEY0,  KEY1,  KEY2
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    //      KEEB: ENCS,  KEY0,  KEY1,  KEY2
-    // QMK ARRAY: KEY0,  KEY2,  KEY1,  ENCS
 [0] = LAYOUT_default(
-    MO(1), KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, KC_SYSTEM_SLEEP),  \
+    // MO(1), KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, KC_SYSTEM_SLEEP),  
+    KC_SYSTEM_SLEEP, MO(1), KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK),  \
 
 [1] = LAYOUT_default(
-    KC_1, RGB_MODE_FORWARD, RGB_TOG, RESET)  \
+    // KC_1, RGB_MODE_FORWARD, RGB_TOG, RESET)  
+    RESET, KC_1, RGB_TOG, RGB_MODE_FORWARD)  \
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
