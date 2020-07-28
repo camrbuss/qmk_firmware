216482a691bb3904f3b69a1ea78df1779e2ecd3b
camrbuss <camrbuss@vt.edu>
2020-07-27 22:47:39 -0600

    Setting default color to use white in RGBW

--- keyboards/kobuss/camro/camro.c
+++ keyboards/kobuss/camro/camro.c
@@ -1,1 +1,10 @@
 #include "camro.h"
+
+#ifdef RGBLIGHT_ANIMATIONS
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
+    rgblight_sethsv_noeeprom(HSV_WHITE);
+
+}
+#endif
